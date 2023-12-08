e14643f5b4c017ed8b2b37460aba32db807abc03
kirito <1554148583@qq.com>
2023-12-08 22:50:57 +0800

    initial project

---
+++ HaremKingv3/HaremKingv3SubModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace HaremKingv3
+{
+    public class HaremKingv3SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            InformationManager.DisplayMessage(new InformationMessage("OnSubModuleLoad"));
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                () => { return (false, null); }
+                )
+            );
+        }
+    }
+}
