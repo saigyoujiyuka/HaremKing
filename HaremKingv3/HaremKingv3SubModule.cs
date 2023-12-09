ad756fa56a7023be0ead3a69ecf09ea3313eeebf
kirito <1554148583@qq.com>
2023-12-09 10:14:12 +0800

    change non-lord character to female

--- HaremKingv3/HaremKingv3SubModule.cs
+++ HaremKingv3/HaremKingv3SubModule.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
+using TaleWorlds.LinQuick;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 
@@ -20,6 +22,51 @@ namespace HaremKingv3
                 () => { return (false, null); }
                 )
             );
+        }
+
+        //protected override void OnApplicationTick(float dt)
+        //{
+        //    base.OnApplicationTick(dt);
+        //}
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            InformationManager.DisplayMessage(new InformationMessage("OnGameStart"));
+        }
+
+        public override void OnGameInitializationFinished(Game game)
+        {
+            base.OnGameInitializationFinished(game);
+            InformationManager.DisplayMessage(new InformationMessage("OnGameInitializationFinished"));
+
+            List<CharacterObject> characterList = game.ObjectManager.GetObjectTypeList<CharacterObject>();
+            InformationManager.DisplayMessage(new InformationMessage("Total: " + characterList.Count.ToString()));
+
+            List<Occupation> excludedOccupations = new List<Occupation>() {
+                Occupation.NotAssigned,
+                Occupation.Lord,
+                Occupation.Special,
+                Occupation.NumberOfOccupations,
+            };
+            List<CharacterObject> femaleCharacterList = characterList.Where(item => !excludedOccupations.Contains(item.Occupation)).ToList();
+            InformationManager.DisplayMessage(new InformationMessage("Female: " + femaleCharacterList.Count.ToString()));
+
+            foreach (CharacterObject characterObject in femaleCharacterList)
+            {
+                if (!characterObject.IsHero)
+                {
+                    characterObject.IsFemale = true;
+                }
+                //if (targetOccupations.Contains(characterObject.Occupation))
+                //{
+                //    characterObject.IsFemale = true;
+                //}
+                //if (characterObject.HeroObject != null)
+                //{
+                //    characterObject.HeroObject.UpdatePlayerGender(true);
+                //}
+            }
         }
     }
 }
