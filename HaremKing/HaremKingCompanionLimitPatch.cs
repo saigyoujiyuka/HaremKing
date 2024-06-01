30fae8790b3b3b21edf200181d4fabcf6183c788
Saigyoji Yuka <lyjlelouch@outlook.com>
2024-06-01 19:51:49 +0800

    Increase companion limit patch

---
+++ HaremKing/HaremKingCompanionLimitPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.GameComponents;
+
+namespace HaremKing
+{
+    [HarmonyPatch(typeof(DefaultClanTierModel), "GetCompanionLimitFromTier")]
+    internal class HaremKingCompanionLimitPatch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result *= 3;
+            return;
+        }
+    }
+}
